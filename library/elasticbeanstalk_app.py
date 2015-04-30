23507370ef9775b51272e7c8f1f23c797b4b1b92
Harpreet Singh <harpreet.singh@pb.com>
2015-04-30 14:56:48 -0400

    Add support for check_mode

--- library/elasticbeanstalk_app.py
+++ library/elasticbeanstalk_app.py
@@ -55,10 +55,38 @@ except ImportError:
 
 
 def describe_app(ebs, app_name):
+    apps = list_apps(ebs, app_name)
+
+    return None if len(apps) != 1 else apps[0]
+
+def list_apps(ebs, app_name):
     apps = ebs.describe_applications(app_name)
     apps = apps["DescribeApplicationsResponse"]["DescribeApplicationsResult"]["Applications"]
 
-    return None if len(apps) != 1 else apps[0]
+    return apps
+
+def check_app(ebs, app, module):
+    app_name = module.params['app_name']
+    description = module.params['description']
+    state = module.params['state']
+
+    result = {}
+
+    if state == 'present' and app is None:
+        result = dict(changed=True, output = "App would be created")
+    elif state == 'present' and app["Description"] != description:
+        result = dict(changed=True, output = "App would be updated", app=app)
+    elif state == 'present' and app["Description"] == description:
+        result = dict(changed=False, output="App is up-to-date", app=app)
+    elif state == 'absent' and app is None:
+        result = dict(changed=False, output="App does not exist")
+    elif state == 'absent' and app is not None:
+        result = dict(changed=True, output="App will be deleted", app=app)
+    elif state == 'list':
+        apps = list_apps(ebs, app_name)
+        result = dict(changed=False, apps=apps)
+
+    module.exit_json(**result)
 
 def main():
     argument_spec = ec2_argument_spec()
@@ -68,7 +96,7 @@ def main():
             state          = dict(choices=['present','absent','list'], default='present')
         ),
     )
-    module = AnsibleModule(argument_spec=argument_spec)
+    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=True)
 
     if not HAS_BOTO:
         module.fail_json(msg='boto required for this module')
@@ -95,6 +123,10 @@ def main():
 
     app = describe_app(ebs, app_name)
 
+    if module.check_mode:
+        check_app(ebs, app, module)
+        module.fail_json('ASSERTION FAILURE: check_app() should not return control.')
+
     if state == 'present':
         if app is None:
             create_app = ebs.create_application(app_name, description)
@@ -118,9 +150,7 @@ def main():
             result = dict(changed=True, app=app)
 
     else:
-        apps = ebs.describe_applications(app_name)
-        apps = apps["DescribeApplicationsResponse"]["DescribeApplicationsResult"]["Applications"]
-
+        apps = list_apps(ebs, app_name)
         result = dict(changed=False, apps=apps)
 
     module.exit_json(**result)
